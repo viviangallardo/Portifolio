6006622c28deaa0901e0ee93f4858521d520d86e
viviangallardo <gallardo@prof.educacao.sp.gov.br>
2025-07-31 20:03:10 -0300

    Criando pagina de portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfólio da Vivian
+
+Este é o portfólio pessoal da Vivian, educadora especializada em Matemática. Neste site você encontrará informações sobre seus projetos, experiências e contato.
---
+++ script.js
@@ -0,0 +1,9 @@
+// Código simples para navegação suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
